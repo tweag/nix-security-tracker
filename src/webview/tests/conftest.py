9703da5430f8c42e0da24703b6092e9a4dee4497
Valentin Gagarin <valentin.gagarin@tweag.io>
2026-01-21 15:59:09 +0100

    refactor: allow fixture to specify user on mock login

--- src/webview/tests/conftest.py
+++ src/webview/tests/conftest.py
@@ -1,16 +1,20 @@
 import os
-from collections.abc import Generator
+from collections.abc import Callable, Generator
 from typing import Any
-from unittest.mock import patch
+from unittest.mock import _patch, patch
 
 import pytest
 from allauth.account.utils import get_login_redirect_url
+from allauth.socialaccount.providers.github.provider import GitHubProvider
 from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.test import Client
+from django.urls import reverse
+from playwright.sync_api import Page
+from pytest_django.live_server_helper import LiveServer
 
 pytest_plugins = ["shared.tests.conftest"]
 
@@ -22,20 +26,35 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 @pytest.fixture
 def mock_oauth_login(
     db: None,
-    user: User,
-) -> Generator[None]:
-    def mock_dispatch(
-        self: OAuth2LoginView, request: HttpRequest, *args: Any, **kwargs: Any
-    ) -> HttpResponse:
-        login(
-            request,
-            user,
-            backend="allauth.account.auth_backends.AuthenticationBackend",
-        )
-        return redirect(get_login_redirect_url(request))
+) -> Callable[[User], _patch]:
+    def wrapped(user: User) -> _patch:
+        def mock_dispatch(
+            self: OAuth2LoginView, request: HttpRequest, *args: Any, **kwargs: Any
+        ) -> HttpResponse:
+            login(
+                request,
+                user,
+                backend="allauth.account.auth_backends.AuthenticationBackend",
+            )
+            return redirect(get_login_redirect_url(request))
 
-    with patch.object(OAuth2LoginView, "dispatch", mock_dispatch):
-        yield
+        return patch.object(OAuth2LoginView, "dispatch", mock_dispatch)
+
+    return wrapped
+
+
+@pytest.fixture
+def as_staff(
+    live_server: LiveServer,
+    mock_oauth_login: Callable[[User], _patch],
+    page: Page,
+    staff: User,
+) -> Generator[Page]:
+    with mock_oauth_login(staff):
+        # XXX(@fricklerhandwerk): Login URLs are "{provider.id}_login":
+        # https://github.com/pennersr/django-allauth/blob/main/allauth/socialaccount/providers/oauth/urls.py#L11
+        page.goto(live_server.url + reverse(f"{GitHubProvider.id}_login"))
+        yield page
 
 
 @pytest.fixture
--- src/webview/tests/test_login.py
+++ src/webview/tests/test_login.py
@@ -1,3 +1,7 @@
+from collections.abc import Callable
+from unittest.mock import _patch
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from playwright.sync_api import Page, expect
 from pytest_django.live_server_helper import LiveServer
@@ -5,13 +9,15 @@ from pytest_django.live_server_helper import LiveServer
 
 def test_redirect_after_login(
     live_server: LiveServer,
-    mock_oauth_login: None,
+    user: User,
+    mock_oauth_login: Callable[[User], _patch],
     page: Page,
 ) -> None:
     entry_point = reverse("webview:suggestions_view")
     page.goto(f"{live_server.url}{entry_point}")
     login_link = page.get_by_role("link", name="Login with GitHub")
     expect(login_link).to_be_visible()
-    login_link.click()
+    with mock_oauth_login(user):
+        login_link.click()
     # We skip the confirmation form in the mock, so the response will be the final redirect
     expect(page).to_have_url(f"{live_server.url}{entry_point}")
