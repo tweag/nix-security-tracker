d57aef7c1a49419672f0b3814612ef84ef3a844c
Florent C. <florentc@users.noreply.github.com>
2026-01-21 16:00:16 +0100

    fix: manually added maintainers not showing in published issues

--- src/shared/github.py
+++ src/shared/github.py
@@ -81,9 +81,10 @@ def create_gh_issue(
         # we have the user id (which is stable), we can ask the GitHub API
         maintainers_list = [
             get_maintainer_username(maintainer, github)
-            for maintainer in cached_suggestion.payload["categorized_maintainers"][
-                "active"
-            ]
+            for maintainer in (
+                cached_suggestion.payload["categorized_maintainers"]["active"] +
+                cached_suggestion.payload["categorized_maintainers"]["added"]
+            )
             if "github_id" in maintainer and "github" in maintainer
         ]
 
