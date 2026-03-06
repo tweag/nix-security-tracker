9e71e5b826a9961f194ce31a85a19f923d01da9b
Florent C. <florentc@users.noreply.github.com>
2026-03-06 16:55:39 +0100

    chore: remove unused "affected list"

--- src/webview/views.py
+++ src/webview/views.py
@@ -11,14 +11,8 @@ from webview.suggestions.views.base import get_suggestion_context
 if typing.TYPE_CHECKING:
     # prevent typecheck from failing on some historic type
     # https://stackoverflow.com/questions/60271481/django-mypy-valuesqueryset-type-hint
-    from django.db.models.query import ValuesQuerySet
+    pass
 
-from django.db.models import (
-    Case,
-    F,
-    Value,
-    When,
-)
 from django.db.models.manager import BaseManager
 from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404
@@ -27,8 +21,6 @@ from django.views.generic import DetailView, ListView, TemplateView
 from shared.models import (
     CveRecord,
     EventType,
-    IssueStatus,
-    NixChannel,
     NixpkgsEvent,
     NixpkgsIssue,
 )
@@ -118,63 +110,5 @@ class NixpkgsIssueListView(ListView):
             issue.github_issue = (
                 github_issue_opened.url if github_issue_opened else None
             )
-
-        return context
-
-
-class NixderivationPerChannelView(ListView):
-    template_name = "affected_list.html"
-    context_object_name = "affected_list"
-    paginate_by = 4
-
-    def _get_ordered_channels(self) -> "ValuesQuerySet[NixChannel, Any]":
-        custom_order = Case(
-            When(state=NixChannel.ChannelState.STABLE, then=Value(1)),
-            When(state=NixChannel.ChannelState.UNSTABLE, then=Value(2)),
-            When(state=NixChannel.ChannelState.DEPRECATED, then=Value(3)),
-            default=Value(4),
-        )
-
-        ordered_channels = (
-            NixChannel.objects.alias(custom_order=custom_order)
-            .filter(custom_order__lt=4)
-            .order_by("custom_order", "channel_branch")
-            .values_list("channel_branch", flat=True)
-        )
-
-        return ordered_channels
-
-    def get_queryset(self) -> Any:
-        channel_filter_value = self.kwargs["channel"]
-        channel = get_object_or_404(NixChannel, channel_branch=channel_filter_value)
-
-        return (
-            NixpkgsIssue.objects.prefetch_related(
-                "cve", "derivations", "derivations__parent_evaluation"
-            )
-            .values(issue_id=F("id"), issue_code=F("code"), issue_status=F("status"))
-            .filter(issue_status=IssueStatus.AFFECTED)
-            .annotate(
-                cve_id=F("cve__id"),
-                cve_code=F("cve__cve_id"),
-                cve_state=F("cve__state"),
-                drv_id=F("derivations__id"),
-                drv_name=F("derivations__name"),
-                drv_system=F("derivations__system"),
-                drv_path=F("derivations__derivation_path"),
-                channel_id=F("derivations__parent_evaluation__channel_id"),
-            )
-            .filter(channel_id=channel.channel_branch)
-        )
-
-    def get_context_data(self, **kwargs: Any) -> Any:
-        context = super().get_context_data(**kwargs)
-        context["channels"] = self._get_ordered_channels()
-        context["current_channel"] = self.kwargs["channel"]
-        context["adjusted_elided_page_range"] = context[
-            "paginator"
-        ].get_elided_page_range(context["page_obj"].number)
-
-        context["headers"] = ["ID", "PLATFORM", "ISSUE", "CVE", "CVE STATE"]
 
         return context
