702a9bc2001d194971127011cae5ea8ebddb47ca
DarshanCode2005 <darshanthakaregsoc2023@gmail.com>
2026-03-18 18:33:30 +0530

    test: unit tests for drv name

---
+++ src/shared/tests/test_utils.py
@@ -0,0 +1,18 @@
+import pytest
+
+from shared.listeners.cache_suggestions import parse_drv_name
+
+
+@pytest.mark.parametrize(
+    "drv_name,expected",
+    [
+        ("openssl-3.0.8", ("openssl", "3.0.8")),
+        ("libfoo-bar-1.2.3", ("libfoo-bar", "1.2.3")),
+        ("foo-1.0-beta", ("foo", "1.0-beta")),
+        ("foo-unstable", ("foo-unstable", "")),
+        ("python3-3.11.0", ("python3", "3.11.0")),
+        ("no-version", ("no-version", "")),
+    ],
+)
+def test_parse_drv_name(drv_name: str, expected: tuple[str, str]) -> None:
+    assert parse_drv_name(drv_name) == expected
