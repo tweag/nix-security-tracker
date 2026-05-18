a1d0cc448a819376b17dc5a6357c8e62d3da3405
Valentin Gagarin <valentin.gagarin@tweag.io>
2026-05-18 12:47:11 +0200

    test: send notification explicitly
    This avoids relying on internal knowledge about the fixtures.
    While we sometimes let that slip, in this case it obsured the mechanism under test.
    
    Co-authored-by: adekoder <adwumiogunbiyi@gmail.com>

--- src/shared/tests/test_notifications.py
+++ src/shared/tests/test_notifications.py
@@ -2,18 +2,19 @@ from collections.abc import Callable
 
 from django.contrib.auth.models import User
 
+from shared.models.linkage import CVEDerivationClusterProposal, ProvenanceFlags
 from shared.models.nix_evaluation import (
     NixDerivation,
     NixMaintainer,
 )
-from webview.models import Notification
+from shared.notify_users import create_package_subscription_notifications
 
 
 def test_github_username_changed(
     user: User,
     make_maintainer_from_user: Callable[..., NixMaintainer],
     make_drv: Callable[..., NixDerivation],
-    make_package_notification: Callable[..., list[Notification]],
+    make_cached_suggestion: Callable[..., CVEDerivationClusterProposal],
 ) -> None:
     """
     Check that notifications get sent even if the user's GitHub handle changed after first login.
@@ -23,5 +24,7 @@ def test_github_username_changed(
     # GitHub username could have changed between evaluations.
     maintainer.github = maintainer.github + "-new"
     maintainer.save()
-    notifications = make_package_notification(drv)
+
+    suggestion = make_cached_suggestion(drvs={drv: ProvenanceFlags.PACKAGE_NAME_MATCH})
+    notifications = create_package_subscription_notifications(suggestion)
     assert notifications
